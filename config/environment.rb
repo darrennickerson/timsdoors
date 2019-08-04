cc10ac3c59535ab2ac9358da291190ef51fd5984
Darren Nickerson <darrennickerson30@gmail.com>
2019-08-04 13:45:33 -0300

    Added credentials

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-    :user_name => SENDGRID_USERNAME,
-    :password => SENDGRID_PASSWORD,
-    :domain => SENDGRID_DOMAIN,
+    :user_name => Rails.application.credentials.sendgrid[:username],
+    :password => Rails.application.credentials.sendgrid[:password],
+    :domain => Rails.application.credentials.sendgrid[:domain],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
