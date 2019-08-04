72a1dd153d216a33e7267a2fbf79a5d1467c509e
Darren Nickerson <darrennickerson30@gmail.com>
2019-08-04 14:58:42 -0300

    switched env vars around

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-ActionMailer::Base.smtp_settings = {
-    :user_name => Rails.application.credentials.sendgrid[:username],
-    :password => Rails.application.credentials.sendgrid[:password],
-    :domain => Rails.application.credentials.sendgrid[:domain],
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => Rails.application.credentials.production[:sendgrid][:username],
+    :password => Rails.application.credentials.production[:sendgrid][:password],
+    :domain => Rails.application.credentials.production[:sendgrid][:domain],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
