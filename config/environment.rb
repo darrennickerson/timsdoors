1192c20e0fe3c7d6123c5fefaf4b596d289d016b
Darren Nickerson <darrennickerson30@gmail.com>
2019-08-04 13:25:00 -0300

    added enviroment vars

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+    :user_name => SENDGRID_USERNAME,
+    :password => SENDGRID_PASSWORD,
+    :domain => SENDGRID_DOMAIN,
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
